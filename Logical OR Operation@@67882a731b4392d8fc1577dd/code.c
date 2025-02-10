1d65d954f7c0b7df69a31dcaa19587d120cf72d6
Anmol-Mittal-2 <anmol0263.becse24@chitkara.edu.in>
2025-02-10 10:55:35 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -3,10 +3,10 @@ int main() {
     int a, b;
     scanf("%d %d", &a , &b);
     if(a>0 || b>0){
-        print("True\n")
+        print("True\n");
     }
     else{
-        print("False\n")
+        print("False\n");
     }
     return 0;
 }
