3f07ac3b0dff87e9e7010240bcbe48bb64987e04
Anmol-Mittal-2 <anmol0263.becse24@chitkara.edu.in>
2025-02-13 23:27:43 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -11,7 +11,7 @@ int main(){
         printf("Vowel\n");
 
     }
-    else if(ch >='a' && ch <='z'){
+    else if((ch >='a' && ch <='z')||(ch>='A' && ch<='Z')){
         printf("Consonant\n");
 
     }
